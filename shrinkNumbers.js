ebb5eef69222ed02a600f9d60a12404cab5d7d95
ecc521 <admin@rivers.run>
2019-10-02 21:26:28 -0400

    Update shrinkNumbers.js

--- shrinkNumbers.js
+++ shrinkNumbers.js
@@ -3,12 +3,21 @@ function shrinkNumber(num) {
 	let exponential = num.toExponential().split("+").join("")
 	let str = num.toString()
 	
+	let shorterExpo = toShorterVersion(num)
+	if (shorterExpo.length <= exponential.length) {
+		if (Number(shorterExpo) === num) {
+			//I don't trust my code...
+			exponential = shorterExpo
+		}
+		else {console.warn("toShorterVersion failed on " + num)}
+	}
+	else {console.warn("toShorterVersion was longer for " + num + ". Native returned " + exponential + ".")}
+	
 	//Although using hexadecimal and octal can sometimes yield smaller representations, JSON does not allow them.
+		
+	//It would also be so nice if we could remove any starting zeroes...
 	
-	//Remove starting 0
-	if (str.startsWith("0.")) {str = str.slice(1)}
-	
-	else if (exponential.length < str.length) {
+	if (exponential.length < str.length) {
 		return exponential
 	}
 	else {
@@ -17,20 +26,22 @@ function shrinkNumber(num) {
 }
 
 
-
-//My attempt to parse numbers
-//Not sure if it fully works - I didn't finish testing negative exponents.
-//Not exported. Kept for historical purposes, or if some use case found in future.
+//My attempt to convert numbers to exponential form. Usually creates shorter outputs than built in designs.
 function toShorterVersion(num) {
+	
+	if (Object.is(0, num)) {return "0"}
+	if (Object.is(-0, num)) {return "-0"}
+
 	let str = num.toString()
 
 	if (str.includes("e")) {
-		//Remove the + from 1e+21 - plus is inferred.
+		//Ideally we would still be able to parse these.
+		//Remove the + if present - plus is inferred.
 		return str.split("+").join("")
 	}
 	
 	//Has to be greater than or equal to 1000 to be able to save space
-	if (num >= 1e3) {
+	if (Math.abs(num) >= 1e3) {
 		//Match trailing zeros
 		let match = /0+$/.exec(str)
 
@@ -44,9 +55,9 @@ function toShorterVersion(num) {
 		}
 	}
 	//Has to be less than or equal to 0.009 to be able to save space. 
-	else if (num <= 9e-3) {
+	else if (Math.abs(num) <= 9e-3) {
 		//Match zeros after decimal point.
-		let match = /.0+/.exec(a)
+		let match = /.0+/.exec(str)
 		if (match) {
 			//Remove the . from the matched string
 			match[0] = match[0].slice(1)
@@ -54,17 +65,86 @@ function toShorterVersion(num) {
 		}
 		let leadingZeros = (match && match[0].length) || 0
 		let matchIndex = (match && match["index"]) || 0
-		
-		//1-2 is bigger, 3 is even, 4+ is smaller.
-		if (leadingZeros > 3) {
+
+		//1 is even, 2+ is smaller. 
+		if (leadingZeros > 1) {
 			let ending = str.slice(matchIndex + leadingZeros)
-			return ending + "e-" + leadingZeros + 1 //Add 1 to leadingZeros - because ending is 1 digit AFTER them.
+			return ending + "e-" + (leadingZeros + ending.length)
 		}
 	}
 	return str
 }
 
 
+//A JSON.stringify polyfill modified to insert shrunk numbers.
+
+var toString = Object.prototype.toString;
+var isArray = Array.isArray || function(a) {
+	return toString.call(a) === '[object Array]';
+};
+var escMap = {
+	'"': '\\"',
+	'\\': '\\\\',
+	'\b': '\\b',
+	'\f': '\\f',
+	'\n': '\\n',
+	'\r': '\\r',
+	'\t': '\\t'
+};
+var escFunc = function(m) {
+	return escMap[m] || '\\u' + (m.charCodeAt(0) + 0x10000).toString(16).substr(1);
+};
+var escRE = /[\\"\u0000-\u001F\u2028\u2029]/g;
+
+
+function stringify(value) {
+	if (value == null) {
+		return 'null';
+	}
+	else if (typeof value === 'number') {
+		return isFinite(value) ? shrinkNumber(value) : 'null';
+	}
+	else if (typeof value === 'boolean') {
+		return value.toString();
+	}
+	else if (typeof value === 'object') {
+		if (typeof value.toJSON === 'function') {
+			return stringify(value.toJSON());
+		}
+		else if (isArray(value)) {
+			var res = '[';
+			for (var i = 0; i < value.length; i++)
+				res += (i ? ',' : '') + stringify(value[i]);
+			return res + ']';
+		}
+		else if (toString.call(value) === '[object Object]') {
+			var tmp = [];
+			for (var k in value) {
+				if (value.hasOwnProperty(k))
+					tmp.push(stringify(k) + ':' + stringify(value[k]));
+			}
+			return '{' + tmp.join(',') + '}';
+		}
+	}
+	return '"' + value.toString().replace(escRE, escFunc) + '"';
+};
+
+
+
+function verifiedStringify(value) {
+	//Quite a bit slower, this function simply makes sure the shrunk object works correctly. (it is possible some technically correct objects
+	//will not be shrunk because the underlying platform moves around the order of object keys, etc.)
+	let shrunk = stringify(value)
+	let native = JSON.stringify(value)
+	
+	if (JSON.stringify(JSON.parse(shrunk)) !== native) {
+		return native
+	}
+	return shrunk
+}
+
 module.exports = {
-  shrinkNumber
+	stringify,
+	verifiedStringify,
+	shrinkNumber
 }
